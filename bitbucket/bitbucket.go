de28e448fb6e38b8997e970f8c3bdd77b689b690
Stanislav Mekhonoshin <ejabberd@gmail.com>
2017-06-16 19:00:53 +0300

    Fix bitbucket

--- bitbucket/bitbucket.go
+++ bitbucket/bitbucket.go
@@ -7,6 +7,6 @@ import (
 func CreateRemoteRepo(repoName, username, appPassword string) {
 	// TODO: remove error if repo not created
 	// TODO: check if repo exists
-	cmd := "curl --user" + username + ":" + appPassword + "-X POST -H \"Content-Type: application/json\" -d '{\"scm\": \"git\"}' https://api.bitbucket.org/2.0/repositories/" + username + "/" + repoName
+	cmd := "curl --user " + username + ":" + appPassword + " -X POST -H \"Content-Type: application/json\" -d '{\"scm\": \"git\"}' https://api.bitbucket.org/2.0/repositories/" + username + "/" + repoName
 	shell.RunShellCmd(cmd)
 }
